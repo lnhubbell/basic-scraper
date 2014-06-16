e8e409f6a4d28db21ec7b03217c026bc5ca8089f
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-06-16 15:30:47 -0700

    wrote initial tests

--- test_scraper.py
+++ test_scraper.py
@@ -1,7 +1,19 @@
 from scraper import *
+import os
+
 
 def test_write_read():
-    to_write = fetch_search_results(minAsk=200)
+
+    to_write = "this is a bunch of test data"
     write_to_file('test.html', to_write)
     html, encoding = read_from_file('test.html')
+    print html
+    os.remove('test.html')
     assert html is not None
+    assert html == "this is a bunch of test data"
+
+
+def test_parse():
+    afile = read_from_file('apartments.html')
+    parsed = parse_source(afile)
+    assert parsed.find()
