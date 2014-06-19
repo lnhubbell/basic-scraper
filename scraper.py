821d7c64a5541a18bdefb7c55d48d7b80bdd3247
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-06-18 17:45:18 -0700

    reduced the code footprint by utillizing yield instead of return

--- scraper.py
+++ scraper.py
@@ -55,20 +55,23 @@ def parse_source(html, the_encoding='utf-8'):
 
 def extract_listings(parsed):
     # location_attrs = {'data-latitude': True, 'data-longitude': True}
-    listings = parsed.find_all('p', class_='row')  # , attrs=location_attrs)
-    extracted = []
+    listings = parsed.find_all('p', class_='row')
+    # delete the line where you create a list in which to store
+    # your listings
     for listing in listings:
+        # location = {key: listing.attrs.get(key, '') for key in location_attrs}
         link = listing.find('span', class_='pl').find('a')
         price_span = listing.find('span', class_='price')
         this_listing = {
+            # 'location': location,
             'pid': listing.attrs.get('data-pid', ''),
             'link': link.attrs['href'],
             'description': link.string.strip(),
             'price': price_span.string.strip(),
             'size': price_span.next_sibling.strip(' \n-/')
         }
-        extracted.append(this_listing)
-    return extracted
+        # delete the line where you append this result to a list
+        yield this_listing  # This is the only change you need to make
 
 
 def add_location(listing, search):
@@ -111,13 +114,16 @@ if __name__ == '__main__':
             minAsk=500, maxAsk=1000, bedrooms=2
         )
     doc = parse_source(html, encoding)
+    # print doc
     json_res = fetch_json_results(minAsk=500, maxAsk=1000, bedrooms=2)
     search = {j['PostingID']:j for j in json_res[0]}
     for listing in extract_listings(doc):
         if (add_location(listing, search)):
             listing = add_address(listing)
             pprint.pprint(listing)
+        else:
+            pass# print "add location failed"
 
 
 # if __name__ == '__main__':
-#     write_to_file('apartments.html', fetch_json_results_for_write(minAsk=500, maxAsk=1000, bedrooms=2))
+#     write_to_file('apartments.html', fetch_search_results(minAsk=500, maxAsk=1000, bedrooms=2))
